f67238da36fadc9a3ef81352cbd00e20017f5e4a
macaureldev <ahouandjinoumarcaurel10@gmail.com>
2025-03-03 11:30:57 +0100

    feat: update main method to test fonctionnalities

--- models/main.java
+++ models/main.java
@@ -1,27 +1,27 @@
 public static void main(String[] args) {
 
-    Contact contactMarc = new Contact("AHOUANDJINOU", "Marc-Aurel", "0158466965", "ahouandjinou@gmail.com");
-    Contact contactIovann = new Contact("ATCHO", "Iovann", "0191323202", "atcho@gmail.com");
-    Contact contactElise = new Contact("AVOKAN", "Elisé", "0158467049", "avokan@gmail.com");
-    Contact contactCynelle = new Contact("HOUKONNOU", "Cynelle", "01600109632", "houkonnou@gmail.com");
+    Contact contact1 = new Contact("Nom1", "Prénom1", "0158466965", "nom1@gmail.com");
+    Contact contact2 = new Contact("Nom2", "Prénom2", "0191323202", "nom2@gmail.com");
+    Contact contact3 = new Contact("Nom3", "Prénom3", "0158467049", "nom3@gmail.com");
+    Contact contact4 = new Contact("Nom4", "Prénom4", "01600109632", "nom4@gmail.com");
 
 
     CarnetAdresse carnetAdr = new CarnetAdresse();
 
-    carnetAdr.addContact(contactMarc);
-    carnetAdr.addContact(contactIovann);
-    carnetAdr.addContact(contactElise);
-    carnetAdr.addContact(contactCynelle);
+    carnetAdr.addContact(contact1);
+    carnetAdr.addContact(contact2);
+    carnetAdr.addContact(contact3);
+    carnetAdr.addContact(contact4);
 
 
     System.out.println("La liste des contact est: ");
     carnetAdr.displayContacts();
 
-    carnetAdr.removeContact("AHOUANDJINOU");
+    carnetAdr.removeContact("Nom3");
     System.out.println("La nouvelle liste de contact est la suivante: ");
     carnetAdr.displayContacts();
 
-    carnetAdr.searchContact("Avokan");
+    carnetAdr.searchContact("Nom2");
 
 }
 
